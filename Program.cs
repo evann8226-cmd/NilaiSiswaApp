7c5919b23ef920618e505bd6eb78c7b05fcd5c25
evann8226-cmd <evann8226@gmail.com>
2026-01-12 09:53:13 +0700

    Tambah input nilai

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿int niali;
+﻿int nilai;
 string nama;
 
 Console.WriteLine("Aplikasi Nili Siswa");
+
+Console.Write("Input Nama: ");
+nama = Console.ReadLine();
+
+Console.Write("IInput NIlai: ");
+nilai = int.Parse(Console.ReadLine());
